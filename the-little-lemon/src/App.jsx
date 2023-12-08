08f174515ae84deeb23ed15be19fbd7262c1e0de
Redouane73 <redouanemessai48@gmail.com>
2023-12-08 05:14:17 -1100

    Another update

--- the-little-lemon/src/App.jsx
+++ the-little-lemon/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">The little lemon</div>;
+  return (
+    <div className="App">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corporis
+        beatae error est sequi officiis laboriosam aliquid ducimus similique
+        esse natus, iure culpa ipsum praesentium eaque totam. Neque, voluptas
+        ut.
+      </p>
+    </div>
+  );
 }
 
 export default App;
